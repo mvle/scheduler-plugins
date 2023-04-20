c55a78f0e28abd23ff3bafe362ee6e2918a1e929
Salman Ahmed <sahmed@ibm.com>
2023-04-19 22:13:03 -0500

    Address reviewer's comments regarding error handling and deleting commented code

--- pkg/sysched/clientset/v1alpha1/spo_client.go
+++ pkg/sysched/clientset/v1alpha1/spo_client.go
@@ -1,15 +1,12 @@
 package v1alpha1
 
 import (
-    //"sigs.k8s.io/security-profiles-operator/api/seccompprofile/v1beta1"
     "k8s.io/apimachinery/pkg/runtime/schema"
-    //"k8s.io/apimachinery/pkg/runtime/serializer"
     "k8s.io/client-go/kubernetes/scheme"
     "k8s.io/client-go/rest"
 )
 
 type SPOV1Alpha1Interface interface {
-    //Profiles(namespace string) ProfileInterface 
     Profiles() ProfileInterface
 }
 
@@ -21,9 +18,7 @@ func NewForConfig(c *rest.Config) (*SPOV1Alpha1Client, error) {
     config := *c
     config.GroupVersion = &schema.GroupVersion{Group: "security-profiles-operator.x-k8s.io", Version: "v1beta1"}
     config.APIPath = "/apis"
-    //crdConfig.NegotiatedSerializer = serializer.NewCodecFactory(scheme.Scheme)
     config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
-    //crdConfig.UserAgent = rest.DefaultKubernetesUserAgent()
     if config.UserAgent == "" {
         config.UserAgent = rest.DefaultKubernetesUserAgent()
     }
